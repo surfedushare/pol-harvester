51f64c9662f8402bfee063cf3da13194f1710ca9
Fako Berkers <email@fakoberkers.nl>
2018-10-05 14:23:50 +0200

    Adds a command to download edurep files and the (open source) task to achieve this with HttpFileResource models

--- datagrowth/resources/http/tasks.py
+++ datagrowth/resources/http/tasks.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from django.apps import apps
 
 from datagrowth.configuration import load_config, DEFAULT_CONFIGURATION
@@ -34,4 +36,20 @@ def send(config, *args, **kwargs):
         has_next_request = current_request = link.create_next_request()
         count += 1
     # Output results in simple type for json serialization
+    return [success, errors]
+
+
+@load_config(defaults=DEFAULT_CONFIGURATION)
+def send_serie(config, args_list, kwargs_list, method=None):
+    success = []
+    errors = []
+    for args, kwargs in zip(args_list, kwargs_list):
+        # Get the results
+        scc, err = send(method=method, config=config, *args, **kwargs)
+        success += scc
+        errors += err
+        # Take a break for scraping if configured
+        interval_duration = config.interval_duration / 1000
+        if interval_duration:
+            sleep(interval_duration)
     return [success, errors]
---
+++ edurep/management/commands/download_edurep.py
@@ -0,0 +1,26 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from datagrowth.resources.http.tasks import send_serie
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        with open("edurep/data/resources.json", "r") as json_file:
+            records = json.load(json_file)
+
+        config = {
+            "resource": "edurep.EdurepFile",
+            "_namespace": "http_resource",
+            "_private": ["_private", "_namespace", "_defaults"]
+        }
+
+        send_serie(
+            [[record["source"]] for record in records],
+            [{} for _ in records],
+            config=config,
+            method="get"
+        )
