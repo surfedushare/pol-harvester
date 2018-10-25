8406059144f9ddad82cadc241de094a6093c4ef8
Fako Berkers <email@fakoberkers.nl>
2018-10-25 11:26:35 +0200

    Plugs Kaldi NL resource into Django

--- pol_harvester/admin.py
+++ pol_harvester/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
 from datagrowth.resources import ResourceAdmin
-from pol_harvester.models import KaldiAspireResource, HttpTikaResource
+from pol_harvester.models import KaldiAspireResource, KaldiNLResource, HttpTikaResource
 
 
 class KaldiAspireResourceAdmin(ResourceAdmin):
+    pass
+
+
+class KaldiNLResourceAdmin(ResourceAdmin):
     pass
 
 
@@ -13,4 +17,5 @@ class HttpTikaResourceAdmin(ResourceAdmin):
 
 
 admin.site.register(KaldiAspireResource, KaldiAspireResourceAdmin)
+admin.site.register(KaldiNLResource, KaldiNLResourceAdmin)
 admin.site.register(HttpTikaResource, HttpTikaResourceAdmin)
--- pol_harvester/models/__init__.py
+++ pol_harvester/models/__init__.py
@@ -1,2 +1,3 @@
 from .kaldi_en import KaldiAspireResource
+from .kaldi_nl import KaldiNLResource
 from .tika import HttpTikaResource
