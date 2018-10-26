51ba1c5978cf4461c7958dab34158512a2d9f06b
Fako Berkers <email@fakoberkers.nl>
2018-10-26 15:12:10 +0200

    Creates dump using Kaldi transcriptions

--- edurep/management/commands/dump_edurep.py
+++ edurep/management/commands/dump_edurep.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 from uuid import uuid4
@@ -5,8 +6,11 @@ from uuid import uuid4
 from django.core.management.base import BaseCommand
 
 from datagrowth.exceptions import DGResourceException
-from pol_harvester.models import HttpTikaResource
+from pol_harvester.models import HttpTikaResource, YouTubeDLResource, KaldiNLResource
 from edurep.models import EdurepFile
+
+
+log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -14,6 +18,28 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-i', '--input', type=str, required=True)
         parser.add_argument('-o', '--output', type=str, required=True)
+
+    def get_text_from_tika(self, record):
+        text = None
+        try:
+            file = EdurepFile().get(record["source"])
+            tika_hash = HttpTikaResource.hash_from_data({"file": file.body})
+            tika_resource = HttpTikaResource.objects.get(data_hash=tika_hash)
+            content_type, content = tika_resource.content
+            text = content.get("text", None)
+        except (DGResourceException, HttpTikaResource.DoesNotExist):
+            pass
+        return text
+
+    def get_text_from_kaldi(self, record):
+        download = YouTubeDLResource().run(record["source"])
+        _, file_path = download.content
+        if file_path is None:
+            log.warning("Could not find download for: {}".format(record["source"]))
+            return None
+        transcription = KaldiNLResource().run(file_path)
+        _, transcript = transcription.content
+        return transcript
 
     def handle(self, *args, **options):
 
@@ -28,15 +54,8 @@ class Command(BaseCommand):
 
         for record in records:
             identifier = str(uuid4())
-            text = None
-            try:
-                file = EdurepFile().get(record["source"])
-                tika_hash = HttpTikaResource.hash_from_data({"file": file.body})
-                tika_resource = HttpTikaResource.objects.get(data_hash=tika_hash)
-                content_type, content = tika_resource.content
-                text = content.get("text", None)
-            except (DGResourceException, HttpTikaResource.DoesNotExist):
-                pass
+            text = self.get_text_from_tika(record) if not record["mime_type"].startswith("video") else \
+                self.get_text_from_kaldi(record)
             output = {
                 "id": identifier,
                 "url": record["source"],
