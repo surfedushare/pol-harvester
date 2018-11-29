31daa3059186cb9daaf8e9e9e2b917d390fb7fb1
Fako Berkers <email@fakoberkers.nl>
2018-11-29 13:51:52 +0100

    Cleans YouTubeDL resources to not contain duplicates

--- surfshare/management/commands/clean_stimuleringsmaatregel_videos.py
+++ surfshare/management/commands/clean_stimuleringsmaatregel_videos.py
@@ -1,5 +1,4 @@
 import os
-import pandas as pd
 import logging
 from tqdm import tqdm
 from datetime import datetime
@@ -20,7 +19,11 @@ class Command(BaseCommand):
         start = datetime(year=2018, month=11, day=15)
         end = datetime(year=2018, month=11, day=30)
         batch_size = 500
-        queryset = YouTubeDLResource.objects.filter(created_at__gte=start, created_at__lt=end)
+        queryset = YouTubeDLResource.objects.filter(
+            created_at__gte=start,
+            created_at__lt=end,
+            uri__contains="youtube.com"
+        )
         count = queryset.count()
         batch_iterator = ibatch(queryset.iterator(), batch_size=batch_size)
         if count >= batch_size * 5:
@@ -30,6 +33,17 @@ class Command(BaseCommand):
             for instance in instances:
                 variables = instance.variables()
                 url = URLObject(variables["url"])
-                url.del_query_param('list')
-                url.del_query_param('index')
-                print(url)
+                url = url.del_query_param('list')
+                url = url.del_query_param('index')
+                if variables["url"] ==  url:
+                    continue
+                instance.command["args"] = [url]
+                instance.uri = instance.uri.replace(variables["url"], url)
+                vid = url.query_dict["v"]
+                content_type, files = instance.content
+                for file in files:
+                    if os.path.exists(file) and file.endswith(vid + ".wav" ):
+                        instance.stdout = "[ffmpeg] Destination: {}".format(file)
+                    elif os.path.exists(file):
+                        os.remove(file)
+                instance.save()
