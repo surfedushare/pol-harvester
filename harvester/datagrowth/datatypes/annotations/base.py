94118ef8b122f95f14034e72bd43a7d67e4b4df7
Fako Berkers <email@fakoberkers.nl>
2019-02-21 13:42:21 +0100

    Open source contribution to make AnnotationBase more useful

--- harvester/datagrowth/datatypes/annotations/base.py
+++ harvester/datagrowth/datatypes/annotations/base.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class AnnotationBase(models.Model):
 
+    user = models.ForeignKey(User, blank=True, null=True)
     reference = models.CharField(max_length=255, db_index=True)
     name = models.CharField(max_length=255, db_index=True)
     value = models.FloatField(blank=True, null=True)
@@ -11,6 +13,9 @@ class AnnotationBase(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    @property
+    def annotation(self):
+        return self.value if self.value is not None else self.string
+
     class Meta:
         abstract = True
-        unique_together = ("reference", "name",)
