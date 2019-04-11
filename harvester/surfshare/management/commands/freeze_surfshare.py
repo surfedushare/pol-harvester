b487c83c27a33eb865694fcb812c2aa2a88f08cc
Fako Berkers <email@fakoberkers.nl>
2019-04-11 16:16:35 +0200

    Adds statistics to freeze surfshare

--- harvester/surfshare/management/commands/freeze_surfshare.py
+++ harvester/surfshare/management/commands/freeze_surfshare.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 from urlobject import URLObject
@@ -5,16 +6,28 @@ from tqdm import tqdm
 
 from pol_harvester.management.base import OutputCommand, FreezeCommand
 from pol_harvester.models import Arrangement
+from pol_harvester.utils.logging import log_header
 from surfshare.constants import VIDEO_DOMAINS
+
+
+out = logging.getLogger("freeze")
 
 
 class Command(FreezeCommand, OutputCommand):
 
     def handle(self, *args, **options):
 
+        log_header(out, "FREEZE SURFSHARE", options)
+
         freeze, collection = self._get_or_create_context(options["freeze"], options["collection"])
 
         for path, dirs, files in os.walk(options["input"]):
+
+            copied = 0
+            videos = 0
+            docs = 0
+            dumped = 0
+
             for file in tqdm(files):
                 with open(os.path.join(path, file), "r") as json_file:
                     data = json.load(json_file)
@@ -34,9 +47,14 @@ class Command(FreezeCommand, OutputCommand):
                             url = url.del_query_param('list')
                             url = url.del_query_param('index')
                             document["url"] = str(url)
-                        documents += self.get_documents_from_kaldi(document)
+                        vids = self.get_documents_from_kaldi(document)
+                        videos += len(vids)
+                        documents += vids
                     else:
-                        documents.append(self._create_document(document["text"], document))
+                        copied += 1
+                        documents.append(self._create_document(document["text"], document, pipeline={"raw": True}))
+                dumped += 1
+                docs += len(documents)
 
                 arrangement = Arrangement.objects.create(
                     freeze=freeze,
@@ -51,3 +69,8 @@ class Command(FreezeCommand, OutputCommand):
                 )
                 if len(documents):
                     arrangement.add(documents, collection=collection)
+
+            out.info("Copied from raw data during dump: {}".format(copied))
+            out.info("Transcribed videos: {}".format(videos))
+            out.info("Dumped Arrangements: {}".format(dumped))
+            out.info("Dumped Documents: {}".format(docs))
