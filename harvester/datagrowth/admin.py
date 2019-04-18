f028ef0d9d894d5990e896b3aed0a79b99b0df86
Fako Berkers <email@fakoberkers.nl>
2019-04-18 17:54:17 +0200

    Adds open source base class for model admin to display DataStorage in admin

--- harvester/datagrowth/admin.py
+++ harvester/datagrowth/admin.py
@@ -1,2 +1,3 @@
 from datagrowth.resources.http.admin import HttpResourceAdmin
 from datagrowth.resources.shell.admin import ShellResourceAdmin
+from datagrowth.datatypes.documents.admin import DataStorageAdmin
