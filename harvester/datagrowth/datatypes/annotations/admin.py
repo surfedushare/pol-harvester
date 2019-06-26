b60554a279d42e3b2bda309aab56876f1e509832
Fako Berkers <email@fakoberkers.nl>
2019-06-26 16:27:33 +0200

    Improves open source annotation admin interface

--- harvester/datagrowth/datatypes/annotations/admin.py
+++ harvester/datagrowth/datatypes/annotations/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 
 class AnnotationAdmin(admin.ModelAdmin):
     list_display = ("reference", "name", "annotation", "user", "created_at", "modified_at")
+    search_fields = ("reference", "string",)
