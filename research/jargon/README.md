622f3a3440d393d5f803bf49ff34c568142e9214
Fako Berkers <email@fakoberkers.nl>
2019-07-03 13:50:54 +0200

    Adds a task that will perform G2P on a vocabulary

--- research/jargon/README.md
+++ research/jargon/README.md
@@ -1,7 +1,7 @@
 Adding jargon to Kaldi NL
 =========================
 
-This directory brings together some code that we use to add vocabulairy to Kaldi NL.
+This directory brings together some code that we use to add vocabulary to Kaldi NL.
 It is mostly a documentation of the steps we took to learn higher education nursery jargon.
 The guide is very basic and not meant to be complete.
 For a more thorough guide checkout: [Artificial Industry docs on Kaldi NL jargon](https://docs.google.com/document/d/1Zq6pZnFX2T5f17zRYALYcP0P1KJ3jBJHgD9AIEaM848/edit#heading=h.oc4yz21n922g).
@@ -22,14 +22,14 @@ Here are the references for these transcripts:
 * f3efd1a94f3212de77a450f3c5aaf4ab38fc97c8
 
 We merged all resulting vocabularies 
-and checked whether the words really did not occur already in the Kaldi NL vocabulairy.
+and checked whether the words really did not occur already in the Kaldi NL vocabulary.
 We identified 29 terms that Kaldi NL does not yet recognize.
-You can find these terms in the [HBOVPK vocabulairy file](vocabularies/hbovpk/vocabulairy.txt).
+You can find these terms in the [HBOVPK vocabulary file](vocabularies/hbovpk/vocabulary.txt).
 
 #### Additional jargon
 
 We identified some jargon that is not present in any of the transcripts by listening to additional videos.
-That list is present in the [extra HBOVPK vocabulairy file](vocabularies/hbovpk/vocabulairy-extra.txt).
+That list is present in the [extra HBOVPK vocabulary file](vocabularies/hbovpk/vocabulary-extra.txt).
 
 
 #### Errors
@@ -38,10 +38,14 @@ There are words that Kaldi NL has in its dictionary, but which were misunderstoo
 One possible reason for this is that the language model is not suitable for the domain we're listening to.
 For instance "wezens" seems to have president over "weefsels", which for a medical application is probably wrong.
 Re-training the language model may help, but is out of scope for this research.
-A complete list of errors can be found in the [HBOVPK error file](vocabularies/hbovpk/vocabulairy-errors.txt).
+A complete list of errors can be found in the [HBOVPK error file](vocabularies/hbovpk/vocabulary-errors.txt).
 
 
 Transforming jargon to phonemes
 -------------------------------
 
+For Dutch we're using a G2P Service. You can invoke the creation of a G2P file through:
 
+```bash
+invoke create-phonemes <vocabulary-name>
+```
---
+++ research/jargon/tasks.py
@@ -0,0 +1,61 @@
+import os
+import random
+import time
+
+import clam.common.client
+import clam.common.data
+import clam.common.status
+from invoke import task
+
+
+def assert_clam_response(client, session_id, response, message, wait=False):
+    if response.errors:
+        print("An error occured: " + response.errormsg)
+        for parametergroup, paramlist in response.parameters:
+            for parameter in paramlist:
+                if parameter.error:
+                    print("Error in parameter " + parameter.id + ": " + parameter.error)
+        client.delete(session_id)
+        raise RuntimeError("CLAM response error: {}".format(message))
+
+    while wait and response.status != clam.common.status.DONE:
+        time.sleep(2)
+        response = client.get(session_id)  # get status again
+        print("\tRunning: " + str(response.completion) + '% -- ' + response.statusmessage)
+
+    return response
+
+
+@task
+def create_phonemes(ctx, vocabulary_name, g2pservice="https://webservices-lst.science.ru.nl/g2pservice"):
+
+    vocabulary_path = os.path.join("vocabularies", vocabulary_name)
+    vocabulary_file_path = os.path.join(vocabulary_path, "vocabulairy.txt")
+    assert os.path.exists(vocabulary_file_path), \
+        "Missing {} for vocabulairy '{}'".format(vocabulary_file_path, vocabulary_name)
+
+    username = os.environ.get("G2PSERVICE_USERNAME")
+    password = os.environ.get("G2PSERVICE_PASSWORD")
+    assert username and password, "Missing credentials for the G2P Service"
+
+    client = clam.common.client.CLAMClient(g2pservice, user=username, password=password, basicauth=True)
+    session_id = "{}_{}".format(vocabulary_name, str(random.getrandbits(64)))
+    print("Getting phonemes for {} under {}".format(vocabulary_name, session_id))
+
+    client.create(session_id)
+    response = client.get(session_id)
+    assert_clam_response(client, session_id, response, "Failed to get a new session/project")
+
+    client.addinputfile(session_id, response.inputtemplate("wordlist"), vocabulary_file_path)
+    response = client.start(session_id)
+    response = assert_clam_response(
+        client, session_id, response,
+        "Failed to start session/project {}".format(session_id),
+        wait=True
+    )
+
+    for output_file in response.output:
+        file_name = "g2p-" + os.path.basename(str(output_file))
+        output_file.copy(os.path.join(vocabulary_path, file_name))
+
+    client.delete(session_id)
