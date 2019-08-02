07ae5fc5d8ef4509fb90a2dfcec0361dfec2e216
Fako Berkers <email@fakoberkers.nl>
2019-08-02 17:03:44 +0200

    Refactors the loading of query rankings to re-use across commands

--- harvester/search/management/commands/analyse_google_search.py
+++ harvester/search/management/commands/analyse_google_search.py
@@ -1,19 +1,16 @@
-import os
-import json
-from collections import OrderedDict
 import requests
-from itertools import chain
 
 from django.core.management import BaseCommand
 
 from datagrowth.exceptions import DGHttpWarning204
 from search.models import GoogleText
-from search.rank_metrics import dcg_at_k
+from search.utils.metrics import dcg_at_k
+from search.utils.rankings import yield_query_rankings
 
 
 class Command(BaseCommand):
 
-    def evaluate_google_results(self, query, results, ranking):
+    def evaluate_google_results(self, results, ranking):
 
         ratings = []
         content_type, data = results.content
@@ -35,31 +32,17 @@ class Command(BaseCommand):
         print(dcg_at_k(ratings, 10))
 
     def handle(self, *args, **options):
-        with open(os.path.join("..", "data", "queries", "queries-en.json")) as en_queries_file:
-            en_queries = json.load(en_queries_file)
-        with open(os.path.join("..", "data", "queries", "queries-nl.json")) as nl_queries_file:
-            nl_queries = json.load(nl_queries_file)
 
-        for query_info in chain(nl_queries, en_queries):
-            query = query_info["queries"][0]
+        for query, ranking in yield_query_rankings():
 
             print(query)
             print("-" * len(query))
-
-            sorted_items = sorted(
-                ((item["hash"], item["rating"],) for item in query_info["items"]),
-                key=lambda item: item[1],
-                reverse=True  # TODO: check if higher is indeed better, it's required for the metric it seems
-            )
-            ranking = OrderedDict(sorted_items)
-
             results = GoogleText()
             try:
                 results = results.get(query)
                 results.close()
             except DGHttpWarning204:
                 print(f"WARNING: no Google search results for '{query}'")
-
-            self.evaluate_google_results(query, results, ranking)
+            self.evaluate_google_results(results, ranking)
             print()
             print()
