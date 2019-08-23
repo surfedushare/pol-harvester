b4bfa8c4dab26337b5438cc0aae57dcec4c86f5a
Fako Berkers <email@fakoberkers.nl>
2019-08-23 17:53:59 +0200

    Updates the DocumentAdmin class

--- harvester/pol_harvester/admin.py
+++ harvester/pol_harvester/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from datagrowth.admin import ShellResourceAdmin, HttpResourceAdmin, DataStorageAdmin, AnnotationAdmin
+from datagrowth.admin import ShellResourceAdmin, HttpResourceAdmin, DataStorageAdmin, DocumentAdmin, AnnotationAdmin
 from pol_harvester.models import (KaldiAspireResource, KaldiNLResource, YouTubeDLResource, HttpTikaResource,
                                   Freeze, Collection, Arrangement, Document, Annotation,
                                   WikipediaCategoryMembers, Corpus, Article)
@@ -22,10 +22,9 @@ class HttpTikaResourceAdmin(HttpResourceAdmin):
     pass
 
 
-class DocumentAdmin(DataStorageAdmin):
+class ExtendedDocumentAdmin(DocumentAdmin):
     list_display = ['__str__', 'freeze', 'collection', 'arrangement', 'created_at', 'modified_at']
     list_filter = ('freeze', 'collection',)
-    search_fields = ("properties",)
 
 
 admin.site.register(KaldiAspireResource, KaldiAspireResourceAdmin)
@@ -36,7 +35,7 @@ admin.site.register(HttpTikaResource, HttpTikaResourceAdmin)
 admin.site.register(Freeze, DataStorageAdmin)
 admin.site.register(Collection, DataStorageAdmin)
 admin.site.register(Arrangement, DataStorageAdmin)
-admin.site.register(Document, DocumentAdmin)
+admin.site.register(Document, ExtendedDocumentAdmin)
 admin.site.register(Annotation, AnnotationAdmin)
 
 admin.site.register(WikipediaCategoryMembers, HttpResourceAdmin)
