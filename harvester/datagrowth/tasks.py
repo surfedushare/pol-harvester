11067cee6c16e331ac6609ee17dc2646cbf21a0f
Fako Berkers <email@fakoberkers.nl>
2019-08-27 17:36:30 +0200

    Adds shell tasks to the Datagrowth package for auto detection

--- harvester/datagrowth/tasks.py
+++ harvester/datagrowth/tasks.py
@@ -1,1 +1,2 @@
-from datagrowth.resources.http.tasks import send, send_serie, send_mass
+from datagrowth.resources.http.tasks import send, send_serie, send_mass_aggregate
+from datagrowth.resources.shell.tasks import run, run_serie, run_mass_aggregate
