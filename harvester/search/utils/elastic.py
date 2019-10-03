26f4f3165e2afc22f54eb8cb4fb53ca75c384417
Fako Berkers <email@fakoberkers.nl>
2019-10-03 17:47:40 +0200

    A more forgiving timeout for ES health checks during client creation

--- harvester/search/utils/elastic.py
+++ harvester/search/utils/elastic.py
@@ -28,7 +28,7 @@ def get_es_client():
                               port=443,
                               http_compress=True)
     # test if it works
-    if not es_client.cat.health():
+    if not es_client.cat.health(request_timeout=30):
         raise ValueError('Credentials do not work for Elastic search')
     return es_client
 
