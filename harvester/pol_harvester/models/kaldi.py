27d0e10da0aad490e5f138666ba680b52e4a8ae9
Fako Berkers <email@fakoberkers.nl>
2019-11-29 17:46:03 +0100

    Fixes problems with some Kaldi jobs

--- harvester/pol_harvester/models/kaldi.py
+++ harvester/pol_harvester/models/kaldi.py
@@ -6,20 +6,24 @@ from datagrowth.resources import KaldiNLResource as KaldiNL, KaldiAspireResource
 class KaldiNLResource(KaldiNL):
 
     def save(self, *args, **kwargs):
+        # This is a tough cookie
+        # Really not sure why at the first try the Postgres seems to be shutdown and with a second try it always works.
+        # Tried some Postgres setting as well as reconnect packages all to no avail
+        # For now we're simply retrying once and leave it at that
         try:
             super().save(*args, **kwargs)
         except OperationalError:
-            print("Problem with KaldiNL command:", self.uri)
-            self.stdout = ""
             super().save(*args, **kwargs)
 
 
 class KaldiAspireResource(KaldiEN):
 
     def save(self, *args, **kwargs):
+        # This is a tough cookie
+        # Really not sure why at the first try the Postgres seems to be shutdown and with a second try it always works.
+        # Tried some Postgres setting as well as reconnect packages all to no avail
+        # For now we're simply retrying once and leave it at that
         try:
             super().save(*args, **kwargs)
         except OperationalError:
-            print("Problem with KaldiAspire command:", self.uri)
-            self.stdout = ""
             super().save(*args, **kwargs)
