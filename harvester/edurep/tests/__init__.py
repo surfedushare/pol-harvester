bf93b9804d5b7a929f9898d5ddc5599935f2d4e9
Fako Berkers <email@fakoberkers.nl>
2020-03-11 17:05:25 +0100

    Further tests for getting seeds from Edurep

--- harvester/edurep/tests/__init__.py
+++ harvester/edurep/tests/__init__.py
@@ -1,4 +1,4 @@
-from .commands.harvest_edurep_seeds import TestSeedHarvest
+from .commands.harvest_edurep_seeds import TestSeedHarvest, TestSeedHarvestWithHistory
 from .commands.harvest_edurep_basic import TestBasicHarvest
 from .commands.freeze_edurep import TestFreezeNoHistory, TestFreezeWithHistory
 from .utils import TestGetEdurepOAIPMHSeeds
--- harvester/edurep/tests/commands/harvest_edurep_seeds.py
+++ harvester/edurep/tests/commands/harvest_edurep_seeds.py
@@ -3,15 +3,19 @@ from io import StringIO
 from datetime import datetime
 
 from django.test import TestCase
-from django.core.management import call_command
+from django.core.management import call_command, CommandError
 from django.utils.timezone import make_aware
 
 from datagrowth.resources.http.tasks import send
+from pol_harvester.models import Arrangement, Document
 from pol_harvester.constants import HarvestStages
 from edurep.models import EdurepHarvest
 
 
 class TestSeedHarvest(TestCase):
+    """
+    This test case represents the scenario where a harvest is started from t=0
+    """
 
     fixtures = ["freezes-new", "surf-oaipmh-1970-01-01"]
 
@@ -67,4 +71,69 @@ class TestSeedHarvest(TestCase):
             call_command("harvest_edurep_seeds", "--freeze=invalid")
             self.fail("harvest_edurep_seeds did not raise for a freeze without pending harvests")
         except EdurepHarvest.DoesNotExist:
+            pass
+
+    def test_edurep_down(self):
+        out = StringIO()
+        with patch("edurep.management.commands.harvest_edurep_seeds.send", return_value=([], [100],)) as send_mock:
+            try:
+                call_command("harvest_edurep_seeds", "--freeze=delta", "--no-progress", stdout=out)
+                self.fail("harvest_edurep_seeds did not fail when EdurepOAIPMH was returning errors")
+            except CommandError:
+                pass
+
+
+class TestSeedHarvestWithHistory(TestCase):
+    """
+    This test case represents the scenario where a harvest from a previous harvest
+    """
+
+    fixtures = ["freezes-history", "surf-oaipmh-2020-01-01", "freezes-history-pre-delete"]
+
+    def test_edurep_surf(self):
+        # Checking whether end result of the command returned by "handle" matches expectations.
+        # We'd expect one OAI-PMH calls to be made which should be a success.
+        # Apart from the main results we want to check if Datagrowth was used for execution.
+        # This makes sure that a lot of edge cases will be covered like HTTP errors.
+        out = StringIO()
+        with patch("edurep.management.commands.harvest_edurep_seeds.send", wraps=send) as send_mock:
+            call_command("harvest_edurep_seeds", "--freeze=delta", "--no-progress", stdout=out)
+        # Asserting main result (ignoring any white lines at the end of output)
+        stdout = out.getvalue().split("\n")
+        stdout.reverse()
+        result = next((rsl for rsl in stdout if rsl))
+        self.assertEqual(result, "OAI-PMH: 1/1")
+        # Asserting Datagrowth usage
+        self.assertEqual(send_mock.call_count, 1, "More than 1 call to send, was edurep_delen set not ignored?")
+        args, kwargs = send_mock.call_args
+        config = kwargs["config"]
+        self.assertEqual(config.resource, "edurep.EdurepOAIPMH", "Wrong resource used for OAI-PMH calls")
+        self.assertEqual(
+            config.continuation_limit, 1000, "Expected very high continuation limit to assert complete sets"
+        )
+        self.assertEqual(args, ("surf", "2020-02-10"), "Wrong arguments given to edurep.EdurepOAIPMH")
+        self.assertEqual(kwargs["method"], "get", "edurep.EdurepOAIPMH is not using HTTP GET method")
+        # Last but not least we check that the correct EdurepHarvest objects have indeed progressed
+        # to prevent repetitious harvests.
+        surf_harvest = EdurepHarvest.objects.get(source__collection_name="surf")
+        self.assertGreater(
+            surf_harvest.harvested_at,
+            make_aware(datetime(year=2020, month=2, day=10)),
+            "surf set harvested_at should got updated to prevent re-harvest in the future"
+        )
+        edurep_delen_harvest = EdurepHarvest.objects.get(source__collection_name="edurep_delen")
+        self.assertEqual(
+            edurep_delen_harvest.latest_update_at, make_aware(datetime(year=1970, month=1, day=1)),
+            "edurep_delen set harvest got updated while we expected it to be ignored"
+        )
+        # Check if Arrangement and documents got removed correctly
+        try:
+            Arrangement.objects.get(id=73743)
+            self.fail("Old deleted Arrangement did not get removed")
+        except Arrangement.DoesNotExist:
+            pass
+        try:
+            Document.objects.get(id=197515)
+            self.fail("Old deleted Document did not get removed")
+        except Document.DoesNotExist:
             pass
